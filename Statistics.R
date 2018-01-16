52d9da7b5b075c6d1f804de9a8b4971ead4c9c82
wytzev@gmail.com <wytzev@gmail.com>
2018-01-16 17:07:54 +0100

    Deleted a bunch of deprecated files, which can be found in the previous commits for documentation purposes. All the relevant code should be in the notebook. Also updated the readme file.

--- Statistics.R
+++
@@ -1,9 +0,0 @@
-library("Hmisc")
-
-m = cbind(outcome_table_Rel$ID, outcome_table_Source$ID)
-
-cor(m, method="kendall", use="pairwise") 
-
-# Nog even kijken of ik deze toetsen moet gebruiken
-rcorr(as.matrix(outcome_table[,c("agg_rel", "agg_edge", "agg_source", "nConcepts")]), type = "pearson")
-rcorr(as.matrix(outcome_table[,c("agg_rel", "agg_edge", "agg_source", "nConcepts")]), type = "spearman")
